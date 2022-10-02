fcec3342f22f21eed9906a6b39a40fd1bf67de6b
Maria <zhmasha2505@gmail.com>
2022-10-02 12:24:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+//Language button dropdown
+function myFunction() {
+    document.getElementById("myDropdownLang").classList.toggle("show");
+}
+
+//FAQ functions
+//Open answer function
+const faqQuestion = document.querySelectorAll('.faq-q-item');
+const faqAnswer = document.querySelectorAll('.faq-a-item');
+const faqIcon = document.querySelectorAll('.faq-icon');
+
+function selectAnswer() {
+    removeShow();
+    removeRotate();
+    const faqAnswer = document.querySelector(`#${this.id}-answer`);
+    faqAnswer.classList.toggle("show");
+    const faqIcon = document.querySelector(`#${this.id}-icon`);
+    faqIcon.classList.add("rotate");
+}
+
+//Remove show class
+function removeShow() {
+    faqAnswer.forEach(item => item.classList.remove("show"));
+}
+//Rotate icon function
+function removeRotate() {
+    faqIcon.forEach(item => item.classList.remove("rotate"));
+}
+//Click
+faqQuestion.forEach(item => item.addEventListener('click', selectAnswer));
+
+
+
+
